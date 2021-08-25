eb9efff17955bada391f804b345b0aec5a628d6c
Farman Ahmed <36335149+farman99ahmed@users.noreply.github.com>
2021-08-25 21:18:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const connection = require('./database/config');
 const cors = require('cors')
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({
     extended: false
 }));
-
 app.use(express.static(__dirname));
 app.use(cors());
 
@@ -20,6 +20,6 @@ const routes = require('./routes/routes');
 app.use('/', routes);
 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`App is running at port 5000`);
 });
